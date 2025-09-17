8d8a9a80d3488c8e64c8c9adeacc402fd9c8b5d5
Ismael Silva <meloismael827@gmail.com>
2025-09-17 20:30:35 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,188 @@
+
+// -----------------------------------------------------------------------
+
+//Isso é um comentário
+/*Isso é um comentário
+e isso aqui também kakakakakak*/
+
+// JavaScript não é tipado, já o Java sim.
+// Processo de juntar informações = Concatenação
+
+// --------------------------------------------------------------------
+
+// TESTE 1:
+// let a = 1
+// let b = 2
+// let nome = 'Mael'
+// let maiorIdade = true
+// const sobreNome = 'Lima'
+// let soma = a + b
+// console.log('O resultado é: ', soma)
+
+// ---------------------------------------------------------------------------
+
+//MÉDIA:
+// let valor1 = 10
+// let valor2 = 20
+// let média = valor1 + valor2 
+// let média2 = média / 2
+// console.log('A média do seu valor é: ', média2)
+
+// -----------------------------------------------------------------------------
+
+//Utilizando Array:
+//Array = Armazenar informações
+
+// let arrayNome = ['Ismael', 'Joice', 'Sandra']
+// let arrayNumero = [1, 2, 3, 4, 5]
+// console.log(arrayNome[1])
+// console.log('A ', arrayNome[1],'tem',arrayNumero[3], 'anos')
+
+// ------------------------------------------------------------------------------
+
+/* API = rede de objetos
+Objeto = Guarda informações de uma identidade
+JSON = Objeto*/
+
+//Utilizando Objetos:
+
+// let objtNome = {
+//     nome: 'Mael',
+//     sobrenome: 'Silva',
+//     idade: 'true'
+// }
+// console.log(objtNome.nome, objtNome.sobrenome)
+
+// ------------------------------------------------------------------------------------
+
+// IF
+// let numeroUm = 1
+// let numeroDois = 5
+
+// if (numeroUm > numeroDois) {
+//     console.log('numeroUm é maior!')
+    
+// } else {
+//     console.log('numeroDois é maior!')
+// }
+
+// switch (numeroUm) {
+//     case 0:
+//         console.log('Valor 0')
+//         break;
+//     case 1:
+//         console.log('Valor 1')
+//         break;
+//     case 2:
+//         console.log('Valor 2')
+//         break;
+//     default:
+//         console.log('Não entrou no case')
+//         break;
+// }
+// ----------------------------------------------------------------------------
+
+// FOR = Percorrer Array
+
+// for (let index = 0; index <= 5; index++) {
+//     console.log('O index é: ' , index)
+// }
+
+// let aray = ['maria', 'joao', 'lucas']
+
+// for (let index = 0; index < Array.length; index++) {
+//     const element = aray[index];
+//     console.log('O elemento é: ', element)
+// }
+
+// let arrayDados = [10, 20, 30, 40, 50]
+// for (let index = 0; index < arrayDados.length; index++) {
+//     const element = arrayDados[index]
+//     if ( element === 40) {
+//         console.log('O número 40 foi encontrado!! Aí simm!!')
+//         break
+//     }
+// }
+
+// -------------------------------------------------------------------------
+
+// While = Tipo um looping
+// let condition = true
+// while (condition) {
+//     console.log('TRAVA ZAP KKKKKKK')
+//     condition = false
+// }
+
+// ---------------------------------------------------------------------------------
+
+// Dom = Injeção de código no HTML / Manipulação
+
+//  ----------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------
+
+
+
+
+
+// Atividade 17/09/25
+// 1.SOMA:
+// let numeroUm = 1
+// let numeroDois = 2
+// let soma = numeroUm + numeroDois
+// console.log('A soma é: ', soma)
+
+// ----------------------------------------------------------------------------------
+
+// 2.Array:
+// let ArrayFrutas = ['M', 'B', 'M', 'U']
+// console.log(ArrayFrutas[1])
+
+// ----------------------------------------------------------------------------------------
+
+// 3.Objeto: (O CODE ESTÁ DANDO ERRO)
+// let livro = {
+//     titulo: "O Senhos dos Anéis",
+//     autor: "J.R.R. Tolkien",
+//     ano: "1954"
+// }
+// console.log('O seu livro é: ', titulo, 'O autor é: ', autor, 'O ano é: ', ano)
+
+// ------------------------------------------------------------------------------------------
+
+// 4.IF/ELSE:
+let idade = 18
+if (idade => 18) {
+    console.log("Você é maior de idade")
+} else {
+    console.log("Você é menor de idade")
+}
+
+// -------------------------------------------------------------------------------------------
+
+//5. Switch Case (Não consegui fazer)
+//6. Não me recordo
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
