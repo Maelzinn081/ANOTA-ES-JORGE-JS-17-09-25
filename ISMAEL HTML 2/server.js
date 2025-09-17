625b49cddeb7ef229d698b90ea2b8a328d7b510d
Ismael Silva <meloismael827@gmail.com>
2025-09-17 20:13:38 -0300

    Add files via upload

---
+++ ISMAEL HTML 2/server.js
@@ -0,0 +1,144 @@
+
+// -----------------------------------------------------------------------
+
+//Isso é um comentário
+/*Isso é um comentário
+e isso aqui também kakakakakak*/
+
+// JavaScript não é tipado, já o Java sim.
+// Processo de juntar informações = Concatenação
+
+// --------------------------------------------------------------------
+
+// TESTE 1:
+// let a = 1
+// let b = 2
+// let nome = 'Mael'
+// let maiorIdade = true
+// const sobreNome = 'Lima'
+// let soma = a + b
+// console.log('O resultado é: ', soma)
+
+// ---------------------------------------------------------------------------
+
+//MÉDIA:
+// let valor1 = 10
+// let valor2 = 20
+// let média = valor1 + valor2 
+// let média2 = média / 2
+// console.log('A média do seu valor é: ', média2)
+
+// -----------------------------------------------------------------------------
+
+//Utilizando Array:
+//Array = Armazenar informações
+
+// let arrayNome = ['Ismael', 'Joice', 'Sandra']
+// let arrayNumero = [1, 2, 3, 4, 5]
+// console.log(arrayNome[1])
+// console.log('A ', arrayNome[1],'tem',arrayNumero[3], 'anos')
+
+// ------------------------------------------------------------------------------
+
+/* API = rede de objetos
+Objeto = Guarda informações de uma identidade
+JSON = Objeto*/
+
+//Utilizando Objetos:
+
+// let objtNome = {
+//     nome: 'Mael',
+//     sobrenome: 'Silva',
+//     idade: 'true'
+// }
+// console.log(objtNome.nome, objtNome.sobrenome)
+
+// ------------------------------------------------------------------------------------
+
+// IF
+// let numeroUm = 1
+// let numeroDois = 5
+
+// if (numeroUm > numeroDois) {
+//     console.log('numeroUm é maior!')
+    
+// } else {
+//     console.log('numeroDois é maior!')
+// }
+
+// switch (numeroUm) {
+//     case 0:
+//         console.log('Valor 0')
+//         break;
+//     case 1:
+//         console.log('Valor 1')
+//         break;
+//     case 2:
+//         console.log('Valor 2')
+//         break;
+//     default:
+//         console.log('Não entrou no case')
+//         break;
+// }
+// ----------------------------------------------------------------------------
+
+// FOR = Percorrer Array
+
+// for (let index = 0; index <= 5; index++) {
+//     console.log('O index é: ' , index)
+// }
+
+// let aray = ['maria', 'joao', 'lucas']
+
+// for (let index = 0; index < Array.length; index++) {
+//     const element = aray[index];
+//     console.log('O elemento é: ', element)
+// }
+
+// let arrayDados = [10, 20, 30, 40, 50]
+// for (let index = 0; index < arrayDados.length; index++) {
+//     const element = arrayDados[index]
+//     if ( element === 40) {
+//         console.log('O número 40 foi encontrado!! Aí simm!!')
+//         break
+//     }
+// }
+
+// -------------------------------------------------------------------------
+
+// While = Tipo um looping
+// let condition = true
+// while (condition) {
+//     console.log('TRAVA ZAP KKKKKKK')
+//     condition = false
+// }
+
+// ---------------------------------------------------------------------------------
+
+// Dom = Injeção de código no HTML / Manipulação
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
